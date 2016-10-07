617f8597b0ad73c4e5fd87b1a4a3890199a0b62f
SBDavid <billl89@126.com>
2016-10-07 22:37:46 +0800

    add init-param to filter

--- DigestAuthUtils/src/main/java/config/AuthConfig.java
+++ DigestAuthUtils/src/main/java/config/AuthConfig.java
@@ -1,15 +1,15 @@
 package config;
 
 public class AuthConfig {
-	public final static String realm = "test.com";
+	public static String realm = "test.com";
 	
-	public final static int nonceExpireHours = 1;
-	public final static int maxNoncePoolSize = 1000;
+	public static int nonceExpireHours = 1;
+	public static int maxNoncePoolSize = 1000;
 	
-	public final static int maxPreviousHashesSize = 5000;
+	public static int maxPreviousHashesSize = 5000;
 	
 	/**
 	 * Prevent insecure legacy digest auth calls
 	 */
-	public final static boolean disallowLegacyAuth = true;
+	public static boolean disallowLegacyAuth = true;
 }
--- DigestAuthUtils/src/main/java/httpserver/DigestAuthFilter.java
+++ DigestAuthUtils/src/main/java/httpserver/DigestAuthFilter.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -20,12 +21,42 @@ import digestauth.NonceManager;
 public class DigestAuthFilter implements Filter {
 
 	@Override
+	public void init(FilterConfig filterConfig) {
+		String realm = filterConfig.getInitParameter("realm");
+		String nonceExpireHours = filterConfig.getInitParameter("nonceExpireHours");
+		String maxNoncePoolSize = filterConfig.getInitParameter("maxNoncePoolSize");
+		String maxPreviousHashesSize = filterConfig.getInitParameter("maxPreviousHashesSize");
+		String disallowLegacyAuth = filterConfig.getInitParameter("disallowLegacyAuth");
+		
+		if (!StringUtils.isEmpty(realm)) {
+			AuthConfig.realm = realm;
+		}
+		
+		if (!StringUtils.isEmpty(nonceExpireHours)) {
+			AuthConfig.nonceExpireHours = Integer.parseInt(nonceExpireHours);
+		}
+		
+		if (!StringUtils.isEmpty(maxNoncePoolSize)) {
+			AuthConfig.maxNoncePoolSize = Integer.parseInt(maxNoncePoolSize);
+		}
+		
+		if (!StringUtils.isEmpty(maxPreviousHashesSize)) {
+			AuthConfig.maxPreviousHashesSize = Integer.parseInt(maxPreviousHashesSize);
+		}
+		
+		if (!StringUtils.isEmpty(disallowLegacyAuth)) {
+			AuthConfig.disallowLegacyAuth = Boolean.parseBoolean(disallowLegacyAuth);
+		}
+	}
+	
+	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		
 		DigestAuthenticator auth = new DigestAuthenticator();
 		
 		String res = auth.authenticate((HttpServletRequest)request, (HttpServletResponse)response);
 		
+		// if user has not been authorized, let user input userName and password
 		if (StringUtils.isEmpty(res) || res.equals("authRequird")) {
 			
 			String nonceStr = NonceManager.add();
@@ -38,6 +69,7 @@ public class DigestAuthFilter implements Filter {
 			((HttpServletResponse)response).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 			
 		}
+		// the password dose not match or the nonce is expired
 		else if (res.equals("authFail")){
 			((HttpServletResponse)response).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 			response.setContentType("text/html;charset=GB2312");
